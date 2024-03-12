564585f0d867672f3da14ecee86a8a6b9510487f
혀느현스 <hyuns@hyuns.dev>
2024-03-12 16:51:01 +0900

    fix: fix width

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import HyunsLogo from "@/assets/hyuns.png";
 
 function App() {
   return (
-    <div className="w-[100dvh] h-[100dvh] flex items-center justify-center bg-background">
+    <div className="w-[100dvw] h-[100dvh] flex items-center justify-center bg-background">
       <div className="flex flex-col justify-start items-center gap-3">
         <img src={HyunsLogo} className="size-[64px] rounded-full" alt="" />
         <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
